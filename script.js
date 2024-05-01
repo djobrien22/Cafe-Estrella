196e582258bab5b5c43b5df5f211a8340ed54296
djobrien22 <166075933+djobrien22@users.noreply.github.com>
2024-05-01 17:58:28 -0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-/*
-Author: Darcy O'Brien
-File Name: script.js
-Date: 4/19/2024
-*/
-
-function hamburger() {
-var menu = document.getElementById("menu-links"); if (menu.style.display === "block") { menu.style.display = "none";
-} else { menu.style.display = "block"; } }
